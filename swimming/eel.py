5636d5b34112355c9669bbfcb8bb1aef746e2c20
Charles Seaton <seatonct@gmail.com>
2023-10-05 15:58:14 -0500

    added food and __str__ method

--- swimming/eel.py
+++ swimming/eel.py
@@ -3,8 +3,16 @@ from datetime import date
 
 class Eel:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.swimming = True
+        self.food = food
         self.date_added = date.today()
+
+    def __str__(self):
+        return f"{self.name} is a {self.species} who eats {self.food}."
+
+    def feed(self):
+        print(
+            f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
--- walking/sheep.py
+++ walking/sheep.py
@@ -3,8 +3,17 @@ from datetime import date
 
 class Sheep:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.walking = True
+        self.shift = shift
+        self.food = food
         self.date_added = date.today()
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}, who eats {self.food} and works the {self.shift} shift."
+
+    def feed(self):
+        print(
+            f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
